562f221a71667519f671b516b704ede315f03338
Xu, Yuncheng | Marcus | CNTD <yuncheng.xu@rakuten.com>
2025-03-19 11:54:41 +0800

    Add logo to navigator

--- frontend/src/components/Navigator.tsx
+++ frontend/src/components/Navigator.tsx
@@ -1,7 +1,9 @@
 import { Link } from 'react-router';
+import logo from '../../../assets/logo.png';
 
 export const Navigator = () => (
   <nav>
+    <img src={logo} alt="Mars RSS Reader Logo" style={{ width: '100px' }} />
     <ul>
       <li>
         <Link to="/">Homepage</Link>
